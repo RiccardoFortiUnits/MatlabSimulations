cf81375cdd4db43772b5231c6256d68e8c4a2d5b
rf <riccardo.forti@elettra.eu>
2024-02-01 11:53:50 +0100

    tia3 controller
    with the added offset control

--- Laser_n_PID_initializer.m
+++ Laser_n_PID_initializer.m
@@ -3,7 +3,7 @@ P = 1;
 I = 0.003;
 setPoint = -0.87;
 fsampling = 125e6;
-nDelay = 0150;
+nDelay = ceil(350*.5);
 
 initialWaitTime = 8e-4;
 
@@ -15,6 +15,34 @@ actualLaserNoise = table2array(readtable("C:/Users/lastline/Documents/PID_first_
 actualLaserNoise(:,2) = (actualLaserNoise(:,2) - mean(actualLaserNoise(:,2))) / (5600 * 0.2);
 laserNoiseAvg = mean(actualLaserNoise(:,2));
 fNoiseSampling = 1 / (rpFloor(2,1) - rpFloor(1,1));
-alpha = 0.9998;%exp(-1/fsampling/highPassFilter_RC);
 
 
+tia3_g1 = 20e3;
+tia3_g2 = 4.9;
+
+% 
+% sim('tia3_controller.slx');
+% 
+% 
+% 
+% % Access the saved signal data
+% lnt = out.laserNoise.Data;
+% firstTransient = find(out.laserNoise.Time > 4000/fsampling, 1, 'first');
+% secondTransient = find(out.laserNoise.Time > initialWaitTime, 1, 'first') + firstTransient;
+% lnt(1:(firstTransient-1)) = 0;
+% lnt(firstTransient:(secondTransient-firstTransient)) = lnt(firstTransient:(secondTransient-firstTransient)) - mean(lnt(firstTransient:(secondTransient-firstTransient)));
+% lnt(secondTransient-firstTransient + 1 : secondTransient - 1) = 0;
+% lnt(secondTransient:end) = lnt(secondTransient:end) - mean(lnt(secondTransient:end));
+% 
+% 
+% % Plot the modified data
+% figure;
+% plot(lnt);
+% hold on
+% plot(out.inputNoise.signals(1).values-mean(out.inputNoise.signals(1).values))
+% hold off
+% title('Modified Signal Data');
+% xlabel('Time');
+% ylabel('Amplitude');
+% grid on;
+% 
